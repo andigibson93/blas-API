f70b148bf3ba050b899ebdd728c6e256154964dc
andigibson93 <andicsgibson@gmail.com>
2020-04-23 20:50:19 -0400

    pushing up to heroku

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
 
-app.get('/hello', function (req, res) {
+app.use(express.static('Resources'));
 
+app.get('/hello', function (req, res) {
     res.send({hello: 'world'})
+})
+
+app.get('/all-assets', function(req, res){
+    const files = fs.readdirSync("./Resources");
+    res.send({assest: files})
 })
 
 app.listen(3000)
