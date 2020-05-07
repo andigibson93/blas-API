24701d7aeefbf93f882eb51592ab2b68c1317108
andigibson93 <andicsgibson@gmail.com>
2020-05-07 14:46:50 -0400

    adding categories

--- index.js
+++ index.js
@@ -8,8 +8,22 @@ app.get('/', function (req, res) {
     res.send({hello: 'world'})
 })
 
+//This route is depreciated
 app.get('/all-assets', function(req, res){
     const files = fs.readdirSync("./Resources");
+    res.send({assest: files})
+})
+
+app.get('/categories', function(req, res){
+    const files = fs.readdirSync("./Resources");
+    const filteredCategories = files.filter(directory => fs.lstatSync("./Resources/" + directory).isDirectory());
+    res.send({categories: filteredCategories})
+})
+
+app.get('/categories/:category', function(req, res){
+    let params = req.params
+    let category = params.category
+    const files = fs.readdirSync(`./Resources/${category}`);
     res.send({assest: files})
 })
 
